58e395d588e409eb46de497708f02d56aaba9163
tom-sapletta-com <tom@sapletta.com>
2022-04-22 18:37:33 +0200

    first commit without examples

---
+++ csv-to-txt-filter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# CONTRIBUTION
+## Author: Tom Sapletta
+## Created Date: 19.04.2022
+#
+# EXAMPLES:
+# ./csv-to-txt-filter.sh "domain" "2/out.csv" "1/in.txt" ".pl"
+# ./csv-to-txt-filter.sh "domain" "2/out.csv" "1/in.txt" ".de"
+#
+# CONFIG
+IN=$1
+OUT=$2
+FILTER=$3
+#
+# START
